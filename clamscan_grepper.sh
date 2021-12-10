f39545eb47abd7e882c9cb7863e37c083b1dd29d
JArmandoG <90565560+JArmandoG@users.noreply.github.com>
2021-12-10 13:37:27 -0600

    Update clamscan_grepper.sh

--- clamscan_grepper.sh
+++ clamscan_grepper.sh
@@ -3,4 +3,4 @@
 # Ejemplo:
 
 clamscan_file=$1
-grep FOUND $clamscan_file --color=always | grep -Evi 'eicar|embedded|exploitdb|httpd' | sed 's/MetasploitPayload/Payload/' && echo && echo "------" && echo && tail -f $clamscan_file
+grep FOUND $clamscan_file --color=always | grep -Evi 'omit|searches' | sed 's/omit-or-delete-results//' && echo && echo "------" && echo && tail -f $clamscan_file

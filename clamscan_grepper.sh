bd737c23ce7edf4769dd781efbf351c2465c5a04
JArmandoG <90565560+JArmandoG@users.noreply.github.com>
2021-12-10 13:39:54 -0600

    Update clamscan_grepper.sh

--- clamscan_grepper.sh
+++ clamscan_grepper.sh
@@ -1,6 +1,7 @@
-# USO:
-# script.sh clamscan_fecha.txt
-# Ejemplo:
+# USES:
+# 1.- Run a clamscan and save it to clamscan_results.txt (Redirection or Tee)
+# 2.- clamscan_grepper clamscan_results.txt > Find only "FOUND" results and also show the last summary
+# Example:
 
 clamscan_file=$1
-grep FOUND $clamscan_file --color=always | grep -Evi 'omit|searches' | sed 's/omit-or-delete-results//' && echo && echo "------" && echo && tail -f $clamscan_file
+grep FOUND $clamscan_file --color=always | grep -Evi 'omit|searches' | sed 's/omit-or-delete-results//' && echo && echo "------" && echo && tail $clamscan_file

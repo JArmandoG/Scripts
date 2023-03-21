5f6341373f8ceeea65df38887a70a724432826bd
JArmandoG <90565560+JArmandoG@users.noreply.github.com>
2023-03-20 22:17:56 -0600

    Update get-dcsyncRights.ps1

--- Powershell/get-dcsyncRights.ps1
+++ Powershell/get-dcsyncRights.ps1
@@ -1,4 +1,6 @@
 # Get all the objects that have DCSync rights over the domain
 
+# REQUIRES POWERVIEW.ps1 - https://github.com/PowerShellMafia/PowerSploit/blob/master/Recon/PowerView.ps1
+
 $domainServer = "<IP or FQDN of the DC>"
 Get-ObjectAcl -DistinguishedName "(Get-ADDomain -Server $domainServer).DistinguishedName -ResolveGUIDs | ?{($_.ObjectType -match 'replication-get') -or ($_.ActiveDirectoryRights -match 'GenericAll') -or ($_.ActiveDirectoryRights -match 'WriteDacl')}

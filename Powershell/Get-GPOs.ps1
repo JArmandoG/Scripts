42b8aba272ccc94bf6c26a1f61f9857fcf7f0f8e
JArmandoG <90565560+JArmandoG@users.noreply.github.com>
2023-04-11 20:41:54 -0600

    Update Get-GPOs.ps1

--- Powershell/Get-GPOs.ps1
+++ Powershell/Get-GPOs.ps1
@@ -1,1 +1,14 @@
+# GPO Exporter: This will generate an HTML file for each GPO with all their properties.
 
+# Change the following Path:
+$REPORT_PATH = 'C:\\<PATH>'
+
+# Collect Info
+$AllGPOs = Get-GPO -All
+
+# Process Info
+foreach ($GPO in $AllGPOs) {
+$GPOGUID = $GPO.Id
+$ReportPath = $REPORT_PATH + $GPO.DisplayName + ".html"
+Get-GPOReport -Guid $GPOGUID -ReportType html -Path $ReportPath
+}
